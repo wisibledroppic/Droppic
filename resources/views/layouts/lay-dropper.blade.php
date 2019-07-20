c47f36e21b0e12986951ace03eedfa5c2a4819e2
Basit Ali <basitalikhatri98@gmail.com>
2019-07-20 11:25:27 +0500

    Website preloader enabled

--- resources/views/layouts/lay-dropper.blade.php
+++ resources/views/layouts/lay-dropper.blade.php
@@ -36,7 +36,14 @@
 </head>
 
 <body>
-        
+    <div class="spinner-wrapper">
+        <div class="sk-folding-cube">
+            <div class="sk-cube1 sk-cube"></div>
+            <div class="sk-cube2 sk-cube"></div>
+            <div class="sk-cube4 sk-cube"></div>
+            <div class="sk-cube3 sk-cube"></div>
+        </div>
+    </div>
     <div class="app-container app-theme-white body-tabs-shadow fixed-header fixed-sidebar">
         <div class="app-header header-shadow">
             <div class="app-header__logo">
